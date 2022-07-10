58d233c4322a285bc2ea932dee532ea794091a76
YERVAND <107866399+Yervand123@users.noreply.github.com>
2022-07-10 16:04:54 +0400

    Add files via upload

---
+++ homework 18 JS/main.js
@@ -0,0 +1,37 @@
+// try {       //                                  RangeError
+//     let a = [];
+//     a.length = a.length - 5
+// } catch (error) {
+//     console.log(error);
+// }
+// finally{
+//     let a = [1,4,3,6,4];
+//     a.length = a.length - 5
+// }
+
+
+// try {     //                                    ReferanceError
+//     let y = 1;
+//     console.log(x);
+// } catch (error) {
+//     console.log(error);
+// }finally{
+//     let y = 1;
+//     console.log(y);
+// }
+
+
+// let a = 11;      //                             TypeError
+// try {
+//     a.toUpperCase();
+// } catch (error) {
+//     console.log(error);
+    
+// }
+
+
+
+// function div(a,b){
+//         return a / b ;
+// }
+// console.log(div(15,5));
